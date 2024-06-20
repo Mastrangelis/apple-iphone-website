e87649495223ebe2494742500b8c7fff281cb691
Nikos Mastrangelis <nikos.mastrangelis@zeply.com>
2024-06-20 13:28:36 +0300

    feat: setup sentry monitoring for the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import * as Sentry from "@sentry/react";
+import "./index.css";
+
+Sentry.init({
+  dsn: "https://160274d4bb3d6f8e3dcb4fb81b710ee6@o4507463695663104.ingest.de.sentry.io/4507463697236048",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
